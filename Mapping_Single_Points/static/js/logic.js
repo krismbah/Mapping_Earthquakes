53c5b0d5d0571362440747d6ac18408b05ee70ea
krismbah <krismbah@gmail.com>
2022-07-24 06:20:25 -0400

    updating logic file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//US view, let map = L.map('mapid').setView([40.7, -94.5], 14);
+//Los Angeles view
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// changed marker to a circle.
+/*L.circle([34.0522, -118.2437], {
+  radius: 100
+}).addTo(map);*/
+
+//Updated map marker
+L.circle([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+
 
 // An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows: Create the map object with a center and zoom level.
 /*
@@ -40,7 +58,16 @@ mapbox/satellite-streets-v11
 */
 
 // Updated- We create the tile layer that will be the background of our map.
+/*
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+*/
+
+//Updated to dark map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
